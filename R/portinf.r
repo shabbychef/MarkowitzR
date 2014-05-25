463b3b8621c8fa3453e33a34eb873f35f095e889
Steven E. Pav <shabbychef@gmail.com>
2014-05-24 21:00:09 -0700

    typos

--- R/portinf.r
+++ R/portinf.r
@@ -69,10 +69,10 @@
 #' and features will be multiplied by the weights. Weights should be
 #' inverse volatility estimates. Defaults to homoskedasticity.
 #' @param Jmat an optional \eqn{p_j \times p}{pj x p} matrix of the
-#' subspace in which we constraint portfolios. Defaults essentially to the
+#' subspace in which we constrain portfolios. Defaults essentially to the
 #' \eqn{p \times p}{p x p} identity matrix.
 #' @param Gmat an optional \eqn{p_g \times p}{pg x p} matrix of the
-#' subspace to which we constraint portfolios to have zero covariance. 
+#' subspace to which we constrain portfolios to have zero covariance. 
 #' The rowspace of \code{Gmat} must be spanned by the rowspace of \code{Jmat}.
 #' Defaults essentially to the \eqn{0 \times p}{0 x p} empty matrix.
 #'
